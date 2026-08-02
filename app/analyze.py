9a59da303dd384d70f3fe117155c375340709698
FransXave <agapaf@spu.edu>
2026-08-02 16:18:24 -0700

    reposition client initialization into a analyze_review function in analyze.py

--- app/analyze.py
+++ app/analyze.py
@@ -4,14 +4,11 @@ from openai import AzureOpenAI
 
 load_dotenv()
 
-
-
 def analyze_review(review_text):
-    # create client
     client = AzureOpenAI(
-        azure_endpoint = os.getenv("AZURE_OPENAI_ENDPOINT"),
-        api_key = os.getenv("AZURE_OPENAI_KEY"),
-        api_version = "2024-02-01"
+        azure_endpoint=os.getenv("AZURE_OPENAI_ENDPOINT"),
+        api_key=os.getenv("AZURE_OPENAI_KEY"),
+        api_version="2024-02-01"
     )
 
     instructions = """
@@ -23,14 +20,11 @@ def analyze_review(review_text):
     """
 
     response = client.chat.completions.create(
-        model = os.getenv("AZURE_OPENAI_DEPLOYMENT"),
-        messages = [
+        model=os.getenv("AZURE_OPENAI_DEPLOYMENT"),
+        messages=[
             {"role": "system", "content": instructions},
             {"role": "user", "content": review_text}
         ]
     )
 
     return response.choices[0].message.content
-
-
-
